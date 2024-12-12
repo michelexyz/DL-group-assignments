8c5022c50cb70bcfc1177d102f3dc79372f6cca1
Michele Vannucci <michele.vannucci20@gmail.com>
2024-12-12 23:26:14 +0100

    small additions

--- NN_utils.py
+++ NN_utils.py
@@ -1,5 +1,4 @@
 import numpy as np
-import matplotlib.pyplot as plt
 
 import torch
 from torchvision import datasets, transforms
@@ -117,44 +116,7 @@ def train(model, x_train, y_train, x_val, y_val, optimizer, criterion, epochs=10
 
 
 
-def plot_results(first_epoch_running_loss, train_evaluations, val_evaluations):
 
-    ## Plot first epoch running loss ##
-    plt.figure(figsize=(10, 6))
-    plt.plot(first_epoch_running_loss, label="Single batch-averaged Loss")
-    conv_size = 100
-    ma_loss = np.convolve(first_epoch_running_loss, np.ones(conv_size), 'valid') / conv_size
-    plt.plot(np.arange(len(ma_loss))+conv_size/2, ma_loss, label="Moving Average Loss", color='orangered')
-    plt.xlabel("Batches")
-    plt.ylabel("Loss")
-    plt.tick_params(axis='y', labelcolor='tab:red')
-    plt.legend()
-    plt.show()
 
-    ## Plot training and validation los ##
-    plt.figure(figsize=(10, 6))
-    plt.plot(train_evaluations[:, 0], label='Training Loss', color='lightcoral')
-    plt.plot(val_evaluations[:, 0], label='Validation Loss', color='orangered')
-    plt.xlabel('Epoch')
-    plt.ylabel('Loss')
-    plt.title('Training and Validation Loss Over Epochs')
 
-    # adjust x labels to start from 1 and be integers
-    plt.xticks(np.arange(0, len(train_evaluations) , 1), np.arange(1, len(train_evaluations) + 1, 1))
-    plt.tick_params(axis='y', labelcolor='tab:red')
-    plt.legend()
-    plt.show()
-
-    ## Plot training and validation accuracy ##
-    plt.figure(figsize=(10, 6))
-    plt.plot(train_evaluations[:, 1], label='Training Accuracy', color='lightblue')
-    plt.plot(val_evaluations[:, 1], label='Validation Accuracy', color='dodgerblue')
-    plt.xlabel('Epoch')
-    plt.ylabel('Accuracy')
-    plt.title('Training and Validation Accuracy Over Epochs')
-    plt.xticks(np.arange(0, len(train_evaluations) , 1), np.arange(1, len(train_evaluations) + 1, 1))
-
-    plt.tick_params(axis='y', labelcolor='tab:blue')
-    plt.legend()
-    plt.show()
 
